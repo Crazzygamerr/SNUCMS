c99408b56bb63f62087eaabc29c9d3b57f6212be
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2021-10-20 12:18:04 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/snucms/MainActivity.java
+++ app/src/main/java/com/example/snucms/MainActivity.java
@@ -13,7 +13,86 @@ Tuckshop Food Reservation
 
 /*
 Required components:
+Firebase database
+    Callbob
+    ISC slots
+    Library entries
+    Tuckshop entries
+Notificaitions
+QR code scanner
+ */
 
+/*
+Screens:
+Login
+Main
+Timetable
+    Today's events
+    Add/remove events
+
+Callbob
+    Add issue
+    Issue history (remove issues)
+    Issue tracking
+
+ISC slots
+    List of slots
+        Each item has name, time, total capacity
+    //shows confirmation on click
+    //only displays your slot on booking
+
+Library
+    QR scanner with token entry
+
+Tuckshop
+    Show menu
+    Pending orders
+    Completed orders
+ */
+
+/*
+Class for event
+    Name
+    Time
+    Repeat
+    Reminder time
+    function to cancel notification
+ */
+
+/*
+Class for issue
+    Id
+    Title
+    Desciption
+    Location
+    Tracking
+        Acknowledged
+        * estimated verification time
+        * Verified - User
+        estimated fix time
+        Issue resolved - User and call
+ */
+
+/*
+ISC slots:
+Collections for each slot
+Reset collections every week
+Able to add only one slot
+
+ */
+
+/*
+Library:
+Simple database design for library
+Only appends user data, with the token number
+ */
+
+/*
+Tuckshop:
+Two tables, one for pending and finished orders
+Add new orders to pending
+Finished orders are moved
+Both are displayed seperately per user
  */
 
 public class MainActivity extends AppCompatActivity {
