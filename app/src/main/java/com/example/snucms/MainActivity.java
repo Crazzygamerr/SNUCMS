cee153d68b2e0f7e872ee80136f488fc5cfebabf
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2021-10-20 11:24:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/snucms/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.snucms;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*
+Timetable with Google Calendar Integration and notifications for classes, assignments, ISC slots and other events
+Call bob: Issue tracking, history and more accountability
+ISC slot booking and digital sign in
+Library Digital Sign in using QR Code
+Tuckshop Food Reservation
+*/
+
+/*
+Required components:
+
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
