9dcd8b7cc28561c193f1bd1c7911285d35fd64a2
madhusriram012 <75003175+madhusriram012@users.noreply.github.com>
2021-10-21 10:09:29 +0530

    Add files via upload

--- app/src/main/java/com/example/snucms/MainActivity.java
+++ app/src/main/java/com/example/snucms/MainActivity.java
@@ -1,138 +1,42 @@
-package com.example.snucms;
+package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/*
-Timetable with Google Calendar Integration and notifications for classes, assignments, ISC slots and other events
-Call bob: Issue tracking, history and more accountability
-ISC slot booking and digital sign in
-Library Digital Sign in using QR Code
-Tuckshop Food Reservation
-*/
-
-/*
-Required components:
-Firebase database
-    Callbob
-    ISC slots
-    Library entries
-    Tuckshop entries
-Notifications
-QR code scanner
- */
-
-/*
-Screens:
-Login
-Main
-Timetable
-    Today's events
-    Add/remove events
-
-Callbob
-    Add issue
-    Issue history (remove issues)
-    Issue tracking
-
-ISC slots
-    List of slots
-        Each item has name, time, total capacity
-    //shows confirmation on click
-    //only displays your slot on booking
-
-Library
-    QR scanner with token entry
-
-Tuckshop
-    Show menu
-    Pending orders
-    Completed orders
- */
-
-/*
-Class for event
-    Name
-    Time
-    Repeat
-    Reminder time
-    function to cancel notification
- */
-
-/*
-Class for issue
-    Id
-    Title
-    Desciption
-    Location
-    Tracking
-        Acknowledged
-        * estimated verification time
-        * Verified - User
-        estimated fix time
-        Issue resolved - User and call
- */
-
-/*
-ISC slots:
-Collections for each slot
-Reset collections every week
-Able to add only one slot
-
- */
-
-/*
-Library:
-Simple database design for library
-Only appends user data, with the token number
- */
-
-/*
-Tuckshop:
-Two tables, one for pending and finished orders
-Add new orders to pending
-Finished orders are moved
-Both are displayed seperately per user
- */
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = findViewById(R.id.button10);
-        Button button2 = findViewById(R.id.button11);
-        EditText editText = findViewById(R.id.editTextNumber);
 
-        button1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        firebaseHelper.addLibraryEntry("name", "netid", 1234, 2);
-                    }
-                }
-        );
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
 
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
-        button2.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        String s = editText.getText().toString();
-                        if(s.isEmpty())
-                            return;
-                        long l = Long.parseLong(s);
-                        firebaseHelper.getSlots(l);
-                    }
-                }
-        );
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
     }
 }
